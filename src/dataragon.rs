641de90226ae6d21c4bc731c9cb9f6100d9c8b42
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-09-20 10:59:53 +0300

    extract ecc and crc adding to serialization module

--- src/dataragon.rs
+++ src/dataragon.rs
@@ -2,37 +2,17 @@ extern crate map_in_place;
 
 use map_in_place::MapVecInPlace;
 
-use crate::ecc::encode_with_ecc;
-use crate::objects::{CryptoSecretbox, StoredData};
 use crate::serialization::paranoid_checksum;
 use crate::shamir::create_data_shares;
+use crate::serialization::add_ecc_and_crc;
 use shamirsecretsharing::hazmat::{combine_keyshares, create_keyshares};
+use crate::objects::CryptoSecretbox;
 
 pub fn split(text: &[u8], allowed_data_damage_level: f32, count: u8, threshold: u8) -> (Vec<Vec<u8>>, CryptoSecretbox) {
     // todo remove this method from 2 lines?
     let (shares, secret_box) = create_data_shares(&text[..], count, threshold);
-    return (add_ecc_and_crc(shares, allowed_data_damage_level), secret_box);
-}
-
-pub fn add_ecc_and_crc(shares: Vec<Vec<u8>>, allowed_data_damage_level: f32) -> Vec<Vec<u8>> {
-    return shares.map(|share| {
-        let ecc_len = share.len() * (2 as f32 * allowed_data_damage_level) as usize;
-        let encoded = encode_with_ecc(share.as_slice(), ecc_len);
-
-        let format_version: u8 = 0;
-
-        let stored = StoredData {
-            version: 0,
-            crc_algorithm: 0,
-            crc: Vec::from(&paranoid_checksum(share.as_slice()).to_be_bytes() as &[u8]),
-            ecc_algorithm: 0,
-            ecc: Vec::from(encoded.ecc()),
-            encrypted_algorithm: 0,
-            data: Vec::from(encoded.data()),
-        };
-
-        return bincode::serialize(&stored).unwrap();
-    });
+    let shares_with_crc_and_ecc = shares.map(|share| add_ecc_and_crc(share, allowed_data_damage_level));
+    return (shares_with_crc_and_ecc, secret_box);
 }
 
 // todo realize
--- src/serialization.rs
+++ src/serialization.rs
@@ -1,12 +1,33 @@
 extern crate sha2;
 extern crate crc;
 
-use crc::{crc16};
+use crc::crc16;
 use sha2::{Sha512, Digest};
+use crate::ecc::encode_with_ecc;
+use crate::objects::{CryptoSecretbox, StoredData};
 
 pub fn paranoid_checksum(data: &[u8]) -> u16 {
     let mut hasher = Sha512::new();
     hasher.input(data);
     let result = hasher.result();
     return crc16::checksum_usb(result.as_slice());
+}
+
+pub fn add_ecc_and_crc(share: Vec<u8>, allowed_data_damage_level: f32) -> Vec<u8> {
+    let ecc_len = share.len() * (2 as f32 * allowed_data_damage_level) as usize;
+    let encoded = encode_with_ecc(share.as_slice(), ecc_len);
+
+    let format_version: u8 = 0;
+
+    let stored = StoredData {
+        version: 0,
+        crc_algorithm: 0,
+        crc: Vec::from(&paranoid_checksum(share.as_slice()).to_be_bytes() as &[u8]),
+        ecc_algorithm: 0,
+        ecc: Vec::from(encoded.ecc()),
+        encrypted_algorithm: 0,
+        data: Vec::from(encoded.data()),
+    };
+
+    return bincode::serialize(&stored).unwrap();
 }
