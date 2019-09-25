1a7f74c89e76d0d33f1da9a8f56005a69b23d9ba
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-09-25 21:01:07 +0300

    bad test was removed

--- src/ecc.rs
+++ src/ecc.rs
@@ -45,28 +45,4 @@ mod tests {
             assert_eq!(data, recovered.data());
         }
     }
-
-    #[test]
-    fn ecc_works_with_only_ecc_corruption() {
-        let data = "abc".as_bytes();
-        for allowed_data_damage_level_step in 1..=5 {
-            let allowed_data_damage_level = allowed_data_damage_level_step as f32 * 0.5;
-            let ecc_len = data.len() * (2 as f32 * allowed_data_damage_level) as usize;
-
-            // Encode data
-            let encoded = encode_with_ecc(data, ecc_len);
-
-            // Simulate some transmission errors
-            let mut corrupted = encoded.clone();
-            let corrupt_bytes = (ecc_len as f32 * allowed_data_damage_level) as usize;
-            for i in data.len()..data.len()+corrupt_bytes {
-                corrupted[i] = 0x0;
-            }
-
-            // Try to recover data
-            let recovered = recover_with_ecc(corrupted, ecc_len).unwrap();
-
-            assert_eq!(data, recovered.data());
-        }
-    }
 }
